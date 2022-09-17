09e88165bc8bd46f859d95c6e48ee67344f03d9f
Narciso Junior <boldrin.narciso@gmail.com>
2022-09-17 14:06:34 +0100

    feat: new updates]

--- words.py
+++ words.py
@@ -1,13 +1,11 @@
+from tkinter import E
+
+file = open("words.txt")
+
 def bigger_than_20(word):
     size = len(word)
     if size > 20:
         print(word)
-
-def word_has_no_letter(word, letter):
-    if word.find(letter) != -1:
-        return True
-    else:
-        return False
 
 def count_e_percentage(file):
     has_e = 0
@@ -22,28 +20,74 @@ def count_e_percentage(file):
             dont_have_e = dont_have_e + 1
         else:
             has_e = has_e + 1
-        
-    percetage = dont_have_e / count * 100
 
-def avoids(word_to_check, forbiden_words):
-    for restrict in forbiden_words:
-        if word_has_no_letter(word_to_check, restrict):
-            found_it = False
-        else:
-            found_it = True
-    
-    return found_it
-        
+    percentage = dont_have_e / count * 100
+    print(percentage)
+
+def word_has_no_letter(word, letter):
+    for letter in word:
+        if letter == 'e':
+            return False
+    return True
+
+    #Never pass through here, just for studies
+    if word.find(letter) != -1:
+        return False
+    else:
+        return True
+
+def avoids(word_to_check, forbiden_word):
+    for letter in word_to_check:
+        if letter in forbiden_word:
+            return False
+    return True
+
+def uses_only(word_to_check, letters_list):
+    for letter in word_to_check:
+        if letter not in letters_list:
+            return False
+    return True
+
+def uses_all(word, letters_list):
+    return uses_only(letters_list, word)
+
+def avoids_in_file(file, forbidden_words):
+    for line in file:
+        word_to_check = line.strip()
+        if avoids(word_to_check, forbidden_words):
+            print("There is no forbidden word in this string")
 
 
-file = open("words.txt")
 
-forbiden_words = ["a", "b", "c"]
-word_to_check = "split"
+def exercise_nine_seven(file):
+    i = 0
+    count = 0
+    for line in file:
+        word = line.strip()
+        if len(word) > 6:
+            print(word)
+            print(len(word))
+            for letter in word:
+                print(i)
+                if (word[i] == word[i+1]) and (len(word) != i+2):
+                    if word[i+2] == word[i+3]: 
+                        if word[i+4] == word[i+5]:
+                            return "achei porra"
+                i = i+1
+    return "achei nada"
 
-count_e_percentage(file)
+#count_e_percentage(file)
+print(exercise_nine_seven(file))
 
-if avoids(word_to_check, forbiden_words):
-    print("There is no forbidden word in this string")
-else:
-    print("Tem boi na linha")
+forbidden_words = ["a", "b", "c"]
+#avoids_in_file(file, forbidden_words)
+
+world_to_check = "split"
+letters_list = ["s", "p", "l", "i", "t"]
+#print(uses_only(world_to_check, letters_list))
+
+world_to_check = "aeiou"
+letters_list = ["a", "e", "i", "o", "u"]
+#print(uses_all(world_to_check, letters_list))
+
+
